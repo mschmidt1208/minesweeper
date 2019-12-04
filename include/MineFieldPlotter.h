373cdc536bac196f0a0980f120598389bc6153e3
mschmidt <marcel.schmidt120891@gmail.com>
2019-12-04 14:40:09 +0100

    Adding Class responsible for plotting mine field.

---
+++ include/MineFieldPlotter.h
@@ -0,0 +1,44 @@
+#ifndef __MINEFIELDPLOTTER_H
+#define __MINEFIELDPLOTTER_H
+
+//
+//  MinefieldPlotter.h
+//
+//  created by Marcel Schmidt, 2019-12-04
+//
+//
+
+// SYSTEM INCLUDES
+#include <stdio.h>
+
+
+
+class MineFieldPlotter
+{
+public:
+    // Life Cycle
+    //-----------------
+    MineFieldPlotter();
+    // ~MineFieldPlotter();
+
+private:
+    // Plotting
+    //---------------
+    void plotField();
+
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  // __MINEFIELDPLOTTER_H
---
+++ src/MineFieldPlotter.cpp
@@ -0,0 +1,47 @@
+//
+//  MinefieldPlotter.cpp
+//
+//  created by Marcel Schmidt, 2019-12-04
+//
+//
+
+// HEADER INCLUDE
+#include "MineFieldPlotter.h"
+
+
+//--------------------------------------
+// Life Cycle
+//--------------------------------------
+
+MineFieldPlotter::MineFieldPlotter()
+{
+}
+
+
+//--------------------------------------
+// Plotting
+//--------------------------------------
+
+void MineFieldPlotter::plotField()
+{
+
+}
+
+
+
+
+­
+
+/*
+─
+│
+┌
+┐
+└
+┘
+├
+┤
+┬
+┴
+┼
+*/
