d199334cd05ded0876ff8015050c6197c9495eb4
mschmidt <marcel.schmidt120891@gmail.com>
2019-12-04 15:50:55 +0100

    Adding GameController class.

---
+++ include/GameController.h
@@ -0,0 +1,28 @@
+#ifndef __GAMECONTROLLER_H
+#define __GAMECONTROLLER_H
+
+//
+//  GameController.h
+//
+//  created by Marcel Schmidt, 2019-12-04
+//
+//
+
+
+class GameController
+{
+public:
+// Life Cycle
+//-------------------
+    GameController();
+    ~GameController();
+};
+
+
+
+
+
+
+
+
+#endif  // __GAMECONTROLLER_H
---
+++ src/GameController.cpp
@@ -0,0 +1,24 @@
+//
+//  GameController.cpp
+//
+//  created by Marcel Schmidt, 2019-12-04
+//
+//
+
+// HEADER INCLUDE
+#include "GameController.h"
+
+
+
+//--------------------------------------
+// Life Cycle
+//--------------------------------------
+
+GameController::GameController()
+{
+}
+
+
+GameController::~GameController()
+{
+}
